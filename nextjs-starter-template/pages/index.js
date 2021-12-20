7715151a64f3c3d74018ac941ec7fa19786f790d
Andrew Min <andrewmin8@gmail.com>
2021-12-19 18:59:22 -0800

    init

---
+++ nextjs-starter-template/pages/index.js
@@ -0,0 +1,12 @@
+import Hero from '@/components/Hero';
+import PageShell from '@/components/PageShell';
+
+export default function Home() {
+  return (
+    <>
+      <PageShell>
+        <Hero />
+      </PageShell>
+    </>
+  );
+}
